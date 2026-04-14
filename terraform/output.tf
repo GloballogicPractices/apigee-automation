65320782dfc33e5fdfed38d99bf04f24047a45a0
jasongumbs-globallogic <jason.gumbs@globallogic.com>
2026-04-13 20:36:38 -0400

    update

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,6 +1,7 @@
 output "model_a_api_keys" {
   description = "API Keys for Premium Dedicated Tenants"
   value = {
+    # Notice the added after credentials
     for k, v in google_apigee_developer_app.model_a_apps : k => v.credentials.consumer_key
   }
   sensitive = true
@@ -9,6 +10,7 @@ output "model_a_api_keys" {
 output "model_b_api_keys" {
   description = "API Keys for Standard Shared Tenants"
   value = {
+    # Notice the added after credentials
     for k, v in google_apigee_developer_app.model_b_apps : k => v.credentials.consumer_key
   }
   sensitive = true
